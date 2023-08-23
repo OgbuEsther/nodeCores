e24f3ddf1f3f59d2dfe45973e7b0b33e289473e6
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-08-24 00:05:51 +0100

    FS

--- FS.ts
+++ FS.ts
@@ -17,3 +17,16 @@ console.log(path.basename('/foo/bar/baz/asdf/quux.html'))
 console.log(path.basename('/foo/bar/baz/asdf/quux.html', '.html'))
 //if you set the last parameter to be a wrong ext name , it will return the file name plus the ext name
 // Returns: 'quux' 
+
+
+// The path.dirname() method returns the directory name of a path, similar to the Unix dirname command. Trailing directory separators are ignored, see path.sep.
+
+console.log(path.dirname('/foo/bar/baz/asdf/quux'))
+// Returns: '/foo/bar/baz/asdf' 
+
+
+console.log(path.format({
+    dir: 'C:\\path\\dir',
+    base: 'file.txt',
+  }))
+  // Returns: 'C:\\path\\dir\\file.txt' 
